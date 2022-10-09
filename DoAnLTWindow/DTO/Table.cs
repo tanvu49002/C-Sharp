06f3ca5ed260fab9d23df6ad60af52c76c6bed2c
tanvu49002 <93104466+tanvu49002@users.noreply.github.com>
2022-10-09 11:25:06 +0700

    Add files via upload

---
+++ DoAnLTWindow/DTO/Table.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAnLTWindow.DTO
+{
+    public class Table
+    {
+        public Table(int id, string ten, string trangthai)
+        {
+            ID = id;
+            Name = ten;
+            Status = trangthai;
+        }
+
+        public Table(DataRow row)
+        {
+            ID = (int)row["ID"];
+            Name = row["TEN"].ToString();
+            Status = row["TRANGTHAI"].ToString();
+        }
+        private string status;
+    
+
+        private string name;
+      
+        private int iD;
+
+        public string Status { get => status; set => status = value; }
+        public string Name { get => name; set => name = value; }
+        public int ID { get => iD; set => iD = value; }
+    }
+}
